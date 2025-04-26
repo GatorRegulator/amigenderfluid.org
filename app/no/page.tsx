5d9c0dfa34e244e26ed134983819a67a53fbf323
GatorRegulator <kalebarora@outlook.com>
2025-04-26 14:54:38 -0400

    Create page.tsx

---
+++ app/no/page.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+const NoPage: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", fontFamily: "Arial, Helvetica, sans-serif", padding: "20px" }}>
+      <div>
+        <p
+          style={{
+            fontWeight: "bold",
+            fontSize: "72pt",
+            marginBottom: "40px",
+          }}
+        >
+          no.
+        </p>
+      </div>
+      <div style={{ fontSize: "13pt", lineHeight: "1.66" }}>
+        <p>How do you feel? Were you expecting this?</p>
+        <p>Do you want to try again?</p>
+        <p>
+          Maybe here:{" "}
+          <Link href="/">
+            <a style={{ textDecoration: "underline", color: "#0070f3" }}>yes page</a>
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NoPage;
